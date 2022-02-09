b0a97895f67094956e143b678504c2a2e48a40f7
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-02-09 21:00:47 +0530

    build effect

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,31 +6,6 @@ import {
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  flex: 1;
-  margin: 5px;
-  position: relative;
-  min-width: 280px;
-  height: 350px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #f5fbfd;
-`;
-
-const Circle = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  background-color: white;
-  position: absolute;
-`;
-
-const Image = styled.img`
-  height: 75%;
-  z-index: 2;
-`;
-
 const Info = styled.div`
   opacity: 0;
   width: 100%;
@@ -45,6 +20,35 @@ const Info = styled.div`
   justify-content: center;
   transition: all 0.5s ease;
   cursor: pointer;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  position: relative;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
+`;
+
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
 `;
 
 const Icon = styled.div`
