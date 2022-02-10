7e614ca5958cab422564b8b2923753864321b87b
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-02-10 19:04:34 +0530

    responsive productList

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,6 +5,7 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
@@ -19,19 +20,25 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: "0px 20px", display: "flex", flexDirection: "column" })}
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: "5px 0", width: "80%" })}
 `;
-const Option = styled.option``;
+const Option = styled.option`
+  font-size: 10px;
+  font-weight: 300;
+`;
 
 function ProductList(props) {
   return (
