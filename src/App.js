d3e008c162486b57be4d9eca234ca4e06b46d24a
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-02-10 12:25:46 +0530

    build login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
@@ -6,7 +7,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <>
-      <Register />
+      <Login />
     </>
   );
 }
