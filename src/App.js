0600c8ecbd3e471450ca658a46e779afc32db6d3
Dimuthu1994 <dimuthudiss6@gmail.com>
2022-02-14 11:05:27 +0530

    build navigation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
     <>
       <Router>
         <Routes>
+          <Route
+            path="/register"
+            element={user ? <Navigate to="/" /> : <Register />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/" /> : <Login />}
+          />
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:category" element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </>
